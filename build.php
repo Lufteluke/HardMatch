dbf95864bee9ac3a7e7e4b878c022bfcc661d455
Lufteluke <ratchetfan92@gmail.com>
2014-11-18 03:31:46 +0100

    All forms and stuff

--- build.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "hello world</br>";
-echo $_GET['Motherboard'] .'</br>';
-echo $_GET['PSU'] .'</br>';
-echo $_GET['GPU'] .'</br>';
-echo $_GET['CPU'] .'</br>';
-echo $_GET['RAM'] .'</br>';
-?>
